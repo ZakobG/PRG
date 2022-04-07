4d1b150338e809514dc79392ccc27b6c5bfcbc25
Jacob_ <jakub.gaba@educanet.cz>
2022-04-07 09:56:56 +0200

    RPS
    Added score counting

--- src/RPS.java
+++ src/RPS.java
@@ -9,27 +9,49 @@ public class RPS {
         System.out.println("Čau šéfe, Vítej ve hře kámen nůžky papír.");
         System.out.println("Vyber co dáš - Kámen 0 - Nůžky 1 - Papír 2");
 
-        int mMove = Integer.parseInt(sc.nextLine());
-        if (mMove < 3) {
-            System.out.println(mMove);
-        } else {
-            System.out.println("Zadal jsi možnost, která není navýběr.");
-            System.exit(0);
-        }
+        int oScore = 0;
+        int mScore = 0;
 
-        int oMove = (int) (Math.random() * 3);
-        switch (oMove) {
-            case 0 -> System.out.println("On má kámen.");
-            case 1 -> System.out.println("On má nůžky.");
-            case 2 -> System.out.println("On má papír.");
-        }
+        while (true) {
+            int mMove = Integer.parseInt(sc.nextLine());
+            if (mMove < 3) {
+                System.out.println(mMove);
+            } else {
+                System.out.println("Zadal jsi možnost, která není navýběr!");
+                System.exit(0);
+            }
 
-        if (oMove == mMove) {
-            System.out.println("Dali jste remízu.");
-        } else if (oMove == 0 && mMove == 1 || oMove == 1 && mMove == 2 || oMove == 2 && mMove == 0) {
-            System.out.println("Prohrál jsi.");
-        } else if (oMove == 1 && mMove == 0 || oMove == 0 && mMove == 2 || oMove == 2 && mMove == 1) {
-            System.out.println("Vyhrál jsi.");
+            int oMove = (int) (Math.random() * 3);
+            switch (oMove) {
+                case 0 -> System.out.println("On má kámen!");
+                case 1 -> System.out.println("On má nůžky!");
+                case 2 -> System.out.println("On má papír!");
+            }
+
+
+            if (oMove == mMove) {
+                System.out.println("Dali jste remízu!");
+            } else if (oMove == 0 && mMove == 1 || oMove == 1 && mMove == 2 || oMove == 2 && mMove == 0) {
+                System.out.println("Prohrál jsi!");
+                oScore++;
+                mScore--;
+            } else if (oMove == 1 && mMove == 0 || oMove == 0 && mMove == 2 || oMove == 2 && mMove == 1) {
+                System.out.println("Vyhrál jsi!");
+                oScore--;
+                mScore++;
+            }
+
+            if (mScore == 3) {
+                System.out.println("Vyhrál jsi celý souboj v kámen nůžky papír!");
+                break;
+            }
+            if (oScore == 3) {
+                System.out.println("Prohrál jsi celý souboj v kámen nůžky papír!");
+                break;
+            } else {
+                System.out.println("Skóre je : " + mScore + " " + oScore);
+            }
         }
     }
 }
+
