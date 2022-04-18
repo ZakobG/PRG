68aec1fb3194081cc62bd67ecf795d48975ee79d
Jacob_ <jakub.gaba@educanet.cz>
2022-04-18 17:55:14 +0200

    Java swing

--- src/Ukoly/SButtons.java
+++ src/Ukoly/SButtons.java
@@ -6,12 +6,10 @@ import javax.swing.*;
 
 public class SButtons implements ActionListener {
     private int i = 0;
-
     private JLabel label;
     private JButton plus;
     private JButton minus;
     private JButton move;
-
     Point Random = new Point();
 
     public JPanel getPanel() {
@@ -20,14 +18,11 @@ public class SButtons implements ActionListener {
         plus = new JButton("+1");
         plus.addActionListener(this);
         panel.add(plus);
-
         minus = new JButton("-1");
         minus.addActionListener(this);
         panel.add(minus);
-
         label = new JLabel("" + i);
         panel.add(label);
-
         move = new JButton("Click me");
         move.addActionListener(this);
         panel.add(move);
@@ -39,25 +34,22 @@ public class SButtons implements ActionListener {
         if (e.getSource() == plus) {
             i++;
             label.setText("" + i);
-        }
-        if (e.getSource() == minus) {
+        } if (e.getSource() == minus) {
             i--;
             label.setText("" + i);
+        } if (e.getSource() == move) {
+            move.setLocation(Random);
         }
-        move.setLocation(Random);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Buttons");
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(3);
-
         SButtons counter = new SButtons();
         JPanel counterPanel = counter.getPanel();
-
         Container content = frame.getContentPane();
         content.add(counterPanel);
-
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
