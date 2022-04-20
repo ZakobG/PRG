abae072028837cb5e73c2a2340b332ef5a5efd10
Jacob_ <jakub.gaba@educanet.cz>
2022-04-20 10:09:52 +0200

    Java swing

--- src/Ukoly/SButtons.java
+++ src/Ukoly/SButtons.java
@@ -3,6 +3,7 @@ package Ukoly;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.xml.stream.Location;
 
 public class SButtons implements ActionListener {
     private int i = 0;
@@ -10,7 +11,10 @@ public class SButtons implements ActionListener {
     private JButton plus;
     private JButton minus;
     private JButton move;
-    Point Random = new Point();
+
+    int rx = 230;
+    int ry = 10;
+    boolean ault = true;
 
     public JPanel getPanel() {
         JPanel panel = new JPanel();
@@ -34,17 +38,32 @@ public class SButtons implements ActionListener {
         if (e.getSource() == plus) {
             i++;
             label.setText("" + i);
-        } if (e.getSource() == minus) {
+        }
+        if (e.getSource() == minus) {
             i--;
             label.setText("" + i);
-        } if (e.getSource() == move) {
-            move.setLocation(Random);
+        }
+
+        int max = 700;
+        int min = 1;
+        int range = max - min + 1;
+
+        ault = !ault;
+        if (ault) {
+            rx = 365;
+            ry = 5;
+            move.setLocation(rx, ry);
+        } else {
+            rx = (int) (Math.random() * range) + min;
+            ry = (int) (Math.random() * range) + min;
+            move.setLocation(rx, ry);
         }
     }
 
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("Buttons");
-        frame.setSize(300, 200);
+        frame.setSize(700, 700);
         frame.setDefaultCloseOperation(3);
         SButtons counter = new SButtons();
         JPanel counterPanel = counter.getPanel();
